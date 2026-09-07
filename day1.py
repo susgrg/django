5bb84608056d8ce569df551f6794fc731a45e1ed
rinasus <gurungsushant721@gmail.com>
2026-09-07 15:46:20 +0545

    first day work

---
+++ day1.py
@@ -0,0 +1,15 @@
+firstname = "Sushant"
+lastname = "Gurung"
+
+age = 16
+weight = 60.21
+isOkay = True
+x = True
+
+print(firstname)
+print(lastname)
+
+print(type(firstname))
+print(type(age))
+print(type(weight))
+print(type(isOkay))
