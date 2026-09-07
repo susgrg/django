b34fa0e1dca589d3235c1fd8576435de581359c8
rinasus <gurungsushant721@gmail.com>
2026-09-07 15:59:28 +0545

    day 2 of learning

---
+++ day2.py
@@ -0,0 +1,51 @@
+bio ='''
+I am sushant
+'''
+
+#print(bio)
+#biolength = len(bio)
+#print(biolength)
+
+language = "Python"
+#print(len(language))
+#print(language[-1])
+#print (language[0:5])
+
+title = " Python Django "
+#print(title.strip())
+#print(title.lower())
+#print(title.upper())
+#print(title.split(" , "))
+
+email = " maisi.com "
+#clean = email.strip().lower().split("i")
+#clean = email.strip().lower().upper()
+
+#print(clean)
+
+
+
+#print(10 + 3)   # 13
+#print(10 - 3)   # 7
+#print(10 * 3)   # 30
+#print(10 / 3)   # 3.333... (always float in Python 3)
+#print(10 // 3)  # 3 (floor division)
+#print(10 % 3)   # 1 (modulo)
+#print(2 ** 10)  # 1024 (exponent)
+
+#print("ha" *3)
+#print("hello" + " " + "world")
+
+#print(5 <= 3)
+#print(10 == 10)  
+#print(10 != 10)
+#print(5 >= 2)
+#print(10 > 5)
+
+name = "asha"
+score = 16
+print(f"student: {name}, Score: {score}")
+
+address = "pokhara"
+ward = "12"
+print(f"address: {address}, ward: {ward}")
