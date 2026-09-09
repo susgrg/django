32fd4740708aef12cd98471fc0d663633115eafd
rinasus <gurungsushant721@gmail.com>
2026-09-09 17:19:57 +0545

    day04

--- day04_multiplication.py
+++ day04_multiplication.py
@@ -1,9 +1,10 @@
-number = input("Enter a number betwwen 1 to 12:")
-if int(number) >1 and int(number) <=12:
+number = int(input("Enter a number betwwen 1 to 12:"))
+if (number) >1 and (number) <=12:
     print(number)
     for n in range(13):
-        print(f"{number}: {number} x {n} = {int(number)*n}",end=" ") 
+        print(f"{number}: {number} x {n} = {(number)*n}",end=" ") 
         print(" ")
 else:
-    print("Not Valid")
+    number = int(input("Enter a valid number between 1 to 12:" ))
+    #print("Not Valid")
 
